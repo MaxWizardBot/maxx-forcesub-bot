cc526de77a559b4292e56558d84c7f5ed107660d
MaxWizard1 <68775158+MaxWizard1@users.noreply.github.com>
2020-08-21 12:12:45 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,10 +8,10 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
   else:
-    BOT_TOKEN = "1348217621:AAHc9Ecow0d3B3_rRMx3p_8wejWV5ph7uZo"
-    DATABASE_URL = "mongodb+srv://maxx:maxx@cluster0.hjf1v.mongodb.net/mybotz?retryWrites=true&w=majority"
-    APP_ID = "1495354"
-    API_HASH = "315267adfa423c833da4878cee1940e7"
+    BOT_TOKEN = ""
+    DATABASE_URL = ""
+    APP_ID = ""
+    API_HASH = ""
 
 
 class Messages():
@@ -20,7 +20,7 @@ class Messages():
 
         "**Force Subscribers**\nForce group members to join a specific channel before sending messages in the group.\n\n**Setup**\n**Step 1.** __Add me in a group(in which you are creator of the group) as admin.__\n**Step 2.** __Send__ `/ForceSubscribe {your channel username}`\n**Step 3.** __Add me to your channel as admin.__\n\n`All set ! I mute users who didn't joined your channel and ask them to join channel and unmute themself.`\n\n**Commands**\n\n/ForceSubscribe { off / no / disable} - To stop force subscriber\n\n/ForceSubscribe {Channel Username} - Set the Channel\n\n/ForceSubscribe - Get current Status",
         
-        "**Developed by @viperadnan**\n\nPowered by @viperpunk\nThanks to - @PyroGram @HasibulKobir"
+        "**Developed by @MaxxRider**\n\nPowered by @MaxxPrivate\nThanks to - @PyroGram @HasibulKobir"
       ]
 
       START_MSG = "Hey [{}](tg://user?id={})\nI am a multifunctional group manager bot.\nLearn more at /help"
